a56ac2eced8af07994493a8decb929f892a03855
qiujieqiong@linuxdeepin.com <qiujieqiong@linuxdeepin.com>
2016-04-27 18:53:27 +0800

    Adapt different language for file path name and file name
    Change-Id: If96035d5924e6df7c288437ab6133e91ec63fdf4

--- testSessionBusDesktop.py
+++ testSessionBusDesktop.py
@@ -7,6 +7,13 @@ import os
 import commands
 import time 
 import pyautogui
+import json
+import pprint
+import sys
+
+reload(sys)
+
+sys.setdefaultencoding('utf8')
 
 
 class testSessionBusDesktop(unittest.TestCase):
@@ -21,68 +28,94 @@ class testSessionBusDesktop(unittest.TestCase):
 		cl.session_if2 = dbus.Interface(cl.session_obj2,dbus_interface='com.deepin.dde.daemon.Launcher')
 
 		cl.usr_home = os.path.expanduser('~')
-		print cl.usr_home
-		cl.status1,cl.output1 = commands.getstatusoutput("touch ~/桌面/a.txt")
-		print cl.status1
-		cl.status2,cl.output2 = commands.getstatusoutput("touch ~/桌面/b.txt")
-		print cl.status2
-		cl.a_path = cl.usr_home+u"/桌面/a.txt"
-		cl.b_path = cl.usr_home+u"/桌面/b.txt"
+		print "User home dir:%s\n" % cl.usr_home
+		cl.user_desktop_dir = commands.getoutput("bash -c 'source ~/.config/user-dirs.dirs \
+														&& echo $XDG_DESKTOP_DIR'").decode("utf-8").split("\n")
+		cl.user_desktop_dir = [n for n in cl.user_desktop_dir if len(n.strip()) > 0]
+		cl.user_desktop_dir = "".join(cl.user_desktop_dir)
+		print "User desktop dir:%s\n" % cl.user_desktop_dir
+		
+		cl.touch_a = "touch %s/a.txt" % cl.user_desktop_dir
+		cl.touch_b = "touch %s/b.txt" % cl.user_desktop_dir
+		cl.output1 = commands.getoutput(cl.touch_a)
+		cl.output2 = commands.getoutput(cl.touch_b)
+		cl.a_path = "%s/a.txt" % cl.user_desktop_dir
+		cl.b_path = "%s/b.txt" % cl.user_desktop_dir
 		time.sleep (2)
+		
 
 
 	@classmethod
 	def tearDownClass(cl):
-		cl.status1,cl.output1 = commands.getstatusoutput("rm -f ~/桌面/a.txt")
-		cl.status2,cl.output2 = commands.getstatusoutput("rm -f ~/桌面/b.txt")
-		cl.status2,cl.output2 = commands.getstatusoutput("rm -rf ~/桌面/.deepin_rich_dir_程序启动组")
-		time.sleep (2)
-		pyautogui.hotkey('alt','f4')
+		
+		cl.status1,cl.output1 = commands.getstatusoutput("rm -f "+cl.user_desktop_dir+"/*")
+		cl.status2,cl.output2 = commands.getstatusoutput("rm -rf "+cl.user_desktop_dir+"/.deepin_rich_dir_*")
+		
 		time.sleep (2)
 		pyautogui.hotkey('alt','f4')
 		
+		time.sleep (5)
+		pyautogui.hotkey('alt','f4')
+		
+		
+
 	def testMenuContent(self):
 		
 		if os.path.exists(self.a_path) and os.path.exists(self.b_path):
-			GenMenuContent = self.session_if.GenMenuContent((self.usr_home+u"/桌面/a.txt",
-															self.usr_home+u"/桌面/b.txt"))
+			GenMenuContent = self.session_if.GenMenuContent((self.a_path,
+															self.b_path))
 			self.assertIsNotNone(GenMenuContent)
-			print GenMenuContent
+
 		else:
 			print "E: a.txt and b.txt didn't exist"
+		print GenMenuContent
 		
+		GenMenuContent = json.JSONDecoder().decode(GenMenuContent)
+
+		print json.dumps(GenMenuContent,indent=4,ensure_ascii=False)
+
+		ActivateFileWithTimestamp = self.session_if.ActivateFileWithTimestamp(self.user_desktop_dir+"/b.txt",("a.txt","b.txt"),\
+																				False,0,2)
+		time.sleep (5)
 		HandleSelectedMenuItem = self.session_if.HandleSelectedMenuItem("11")
-		print HandleSelectedMenuItem
-		
+
+		time.sleep (5)
+
 		
 	def testAppGroup(self):
-
+		#send deepin-feedback deepin-movie eog to desktop
 		feedback = self.session_if2.RequestSendToDesktop('deepin-feedback')
-		#time.sleep (5)
+		time.sleep (5)
 		self.assertTrue(feedback)
 		
 		movie = self.session_if2.RequestSendToDesktop('deepin-movie')
-		#time.sleep (5)
+		time.sleep (5)
 		self.assertTrue(movie)
 
 		eog = self.session_if2.RequestSendToDesktop('eog')
-		#time.sleep (5)
+		time.sleep (5)
 		self.assertTrue(eog)
-
-		creatappgroup = self.session_if.RequestCreatingAppGroup((self.usr_home+u"/桌面/deepin-feedback.desktop",\
-			self.usr_home+u"/桌面/deepin-movie.desktop"))
+		#drag deepin-feedback towards deepin-movie 
+		creatappgroup = self.session_if.RequestCreatingAppGroup((self.user_desktop_dir+"/deepin-feedback.desktop",\
+			self.user_desktop_dir+"/deepin-movie.desktop"))
 		self.assertIsNone(creatappgroup)
-		
-		mergeappgroup = self.session_if.RequestMergeIntoAppGroup((self.usr_home+u"/桌面/eog.desktop",\
-			self.usr_home+u"/桌面/.deepin_rich_dir_程序启动组"),self.usr_home+u"/桌面/.deepin_rich_dir_程序启动组")
+		time.sleep (5)
+
+		#get AppGroup name
+		self.user_desktop_appgroup = commands.getoutput("bash -c 'source ~/.config/user-dirs.dirs \
+														&& ls -ahl $XDG_DESKTOP_DIR |grep .deepin_rich_dir_'")\
+														.decode("utf-8").split("\n")
+		self.user_desktop_appgroup = [n for n in self.user_desktop_appgroup if len(n.strip()) > 0]
+		#self.user_desktop_appgroup = str(self.user_desktop_appgroup).replace('u\'','\'').decode("unicode-escape")
+		self.user_desktop_appgroup = "".join(self.user_desktop_appgroup)
+		self.user_desktop_appgroup = self.user_desktop_appgroup.split("  ")[2].split(" ")[2]
+		print "AppGroup name is:%s\n" % self.user_desktop_appgroup
+		#merge eog into AppGroup
+		self.appgroup_dir = "%s/%s" % (self.user_desktop_dir,self.user_desktop_appgroup)
+		mergeappgroup = self.session_if.RequestMergeIntoAppGroup((self.user_desktop_dir+"/eog.desktop",self.appgroup_dir),self.appgroup_dir)
 		self.assertIsNone(mergeappgroup)
+		time.sleep (5)
 		
-		ActivateFileWithTimestamp = self.session_if.ActivateFileWithTimestamp(self.usr_home+u"/桌面/b.txt",("a.txt","b.txt"),\
-																				False,0,2)
-		print ActivateFileWithTimestamp
-
-	
-
 def suite():
 	suite = unittest.TestSuite()
 	suite.addTest(testSessionBusDesktop('testMenuContent'))
