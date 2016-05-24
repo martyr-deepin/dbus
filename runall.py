b960595811b86bc9dac5be94ce40df4069adf380
qiujieqiong@linuxdeepin.com <qiujieqiong@linuxdeepin.com>
2016-05-24 18:10:47 +0800

    Output test result to test.result
    Change-Id: Id664cbeed99640a4652125fcc0a0286839e2c1e6

--- runall.py
+++ runall.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
 import test_session_bus_daemon_Dock_dde_dock_DockSetting
 import testSessionBusDesktop
 import testSessionBusLauncher
 import testSessionBusLauncherSetting
+
 
 suite1 = test_session_bus_daemon_Dock_dde_dock_DockSetting.suite()
 suite2 = testSessionBusLauncher.suite()
@@ -14,5 +16,13 @@ suite4 = testSessionBusLauncherSetting.suite()
 alltests = unittest.TestSuite((suite1,suite2,suite3,suite4))
 
 # 执行测试
+def main(out=sys.stderr, verbosity=2):
+	unittest.TextTestRunner(out,verbosity=verbosity).run(alltests)
+
+if __name__ == '__main__':
+	with open('test.result', 'w') as f:
+		main(f)
+'''
 runner = unittest.TextTestRunner()
 runner.run(alltests)
+'''
