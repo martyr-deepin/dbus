1ef5ea6e9972c95ecece54ed9fd8003cb0cceaab
wangyingtao@deepin.com <wangyingtao@deepin.com>
2017-09-05 08:51:54 +0800

    add sink and source lib
    Change-Id: Iba24e990a4547ffa7e10804a01e903a68215b965

--- lib/com_deepin_daemon_Audio.py
+++ lib/com_deepin_daemon_Audio.py
@@ -49,3 +49,46 @@ class DbusAudio:
         设置默认输入
         """
         self.ifc_methods.SetDefaultSource(source_obj_path)
+
+class DbusAudioSink:
+    def __init__(self, path):
+        self.dbus_name = "com.deepin.daemon.Audio"
+        self.dbus_path  = path
+        self.dbus_ifc = "com.deepin.daemon.Audio.Sink"
+
+        self.session_bus = dbus.SessionBus()
+        self.session_obj = self.session_bus.get_object(self.dbus_name,
+                self.dbus_path)
+        self.ifc_properties = dbus.Interface(self.session_obj,
+                dbus_interface=dbus.PROPERTIES_IFACE)
+        self.ifc_methods    = dbus.Interface(self.session_obj,
+                dbus_interface=self.dbus_ifc)
+
+    def getDefaultSource(self):
+        """
+        获取默认音频输入object path
+        """
+        return self.ifc_properties.Get(self.dbus_ifc,
+                "DefaultSource")
+
+class DbusAudioSource:
+    def __init__(self, path):
+        self.dbus_name = "com.deepin.daemon.Audio"
+        self.dbus_path  = path
+        self.dbus_ifc = "com.deepin.daemon.Audio.Source"
+
+        self.session_bus = dbus.SessionBus()
+        self.session_obj = self.session_bus.get_object(self.dbus_name,
+                self.dbus_path)
+        self.ifc_properties = dbus.Interface(self.session_obj,
+                dbus_interface=dbus.PROPERTIES_IFACE)
+        self.ifc_methods    = dbus.Interface(self.session_obj,
+                dbus_interface=self.dbus_ifc)
+
+    def getDefaultSource(self):
+        """
+        获取默认音频输入object path
+        """
+        return self.ifc_properties.Get(self.dbus_ifc,
+                "DefaultSource")
+
