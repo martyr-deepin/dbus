d68a34fba8c71ebf704fe4fac36e90fa83247b26
wangyingtao@deepin.com <wangyingtao@deepin.com>
2017-12-06 10:37:28 +0800

    update for screen roration lib
    Change-Id: I3118e6934b7236e58b57961956cb396c43fb13b9

--- lib/com_deepin_daemon_Display.py
+++ lib/com_deepin_daemon_Display.py
@@ -19,14 +19,32 @@ class DbusDisplay:
 
     def getMonitors(self):
         """
-        获取显示器列表Object Path
+        获取显示器列表Object Path，以Primary主屏名结尾的是主显示器
         """
         return self.ifc_properties.Get(self.dbus_ifc,
-                "Monitors")
+                                       "Monitors")
+
+    def getPrimary(self):
+        """
+        获取主屏名
+        """
+        return self.ifc_properties.Get(self.dbus_ifc,
+                                       "Primary")
+
+    def ApplyChanges(self):
+        """
+        修改旋转屏幕显示后，需要刷新屏幕显示，使设置生效
+        """
+        try:
+            self.ifc_methods.ApplyChanges()
+            return True
+        except Exception as e:
+            print("Unexpected Error: {}".format(e))
+            return False
 
     def GetBrightness(self):
         """
-        获取亮度值
+        获取各显示器亮度值
         """
         return self.ifc_methods.GetBrightness()
 
@@ -35,7 +53,6 @@ class DbusDisplay:
         设置亮度值
         """
         self.ifc_methods.SetBrightness(d_value)
-
 
 class DbusDisplayMonitorVGA(object_path):
     def __init__(self):
@@ -55,5 +72,27 @@ class DbusDisplayMonitorVGA(object_path):
         """
         获取显示器分辨率数组列表
         """
-        return self.ifc_methods.Get(self.dbus_ifc,
+        return self.ifc_properties.Get(self.dbus_ifc,
                 "Modes")
+
+    def getRotations(self):
+        """
+        获取旋转取值列表, [1, 2, 4, 8]
+        """
+        return self.ifc_properties.Get(self.dbus_ifc,
+                "Rotations")
+
+    def SetRotation(self, rotation):
+        """
+        设置屏幕旋转
+            1 正常
+            2 逆时针旋转90°
+            4 逆时针旋转180°
+            8 逆时针旋转270°
+        """
+        try:
+            self.ifc_methods.SetRotation(rotation)
+            return True
+        except Exception as e:
+            print("Unexpected Error: {}".format(e))
+            return False
